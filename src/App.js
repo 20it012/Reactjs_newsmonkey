77a70ce74ebca26e2ce0e601b1e8439fc0308559
divyesh <20it012@charusat.edu.in>
2022-09-13 13:13:52 +0530

    added top-loadingBar, React-router-dom, add routers,routes,route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
-
+import Navbar from './Components/NavBar';
+// import { Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import React, { Component } from 'react'
-import NavBar from './Components/NavBar';
 import News from './Components/News';
+import LoadingBar from 'react-top-loading-bar';
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
   render() {
     return (
       <div>
-        <NavBar/>
-        <News/>
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress={this.state.progress}
+          />
+          <Routes>
+            <Route exact path="/"><News setProgress={this.setProgress} key="general" pageSize={5} country="in" category="general" /></Route>
+            <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={9} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={9} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News setProgress={this.setProgress} key="general" pageSize={9} country="in" category="general" /></Route>
+            <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={9} country="in" category="health" /></Route>
+            {/* <Route path='/about'><About/></Route> */}
+          </Routes>
+        </Router>
       </div>
     )
   }
